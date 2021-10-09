38972389feb34fff4ea76748eaff44be02fa8a21
Marlon <91838856+mveuse@users.noreply.github.com>
2021-10-09 16:08:11 +0200

    Marlon changings

--- src/nishigaki/HelloFromYuki.java
+++ src/nishigaki/HelloFromYuki.java
@@ -4,6 +4,8 @@ public class HelloFromYuki {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Yuki Nishigaki.");
+        
+        System.out.println("Hi Yuki, this Marlon, and I changed your code.");
 
     }
 
